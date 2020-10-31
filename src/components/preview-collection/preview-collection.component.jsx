38cf4f128a3449a1e60dc94b9b407e1f9566cf05
Stefan B <stefanbolcs@gmail.com>
2020-10-31 20:25:17 +0100

    init store

---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './preview-collection.styles.scss';
+
+const CollectionPreview = () => (
+    <div className='collection-preview'>
+<h1 className='title'>Title</h1>
+<div className = 'preview'>
+
+</div>
+
+    </div>
+)
